4fd7b2e41f34cbb37e724678f1244d71fcf4b564
Ryan Zolper <zolper11@gmail.com>
2015-03-05 16:24:48 -0500

    shgsg

--- db/schema.rb
+++ db/schema.rb
@@ -61,7 +61,6 @@ ActiveRecord::Schema.define(version: 20150303225415) do
     t.string   "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string   "pinimage"
   end
 
 end
