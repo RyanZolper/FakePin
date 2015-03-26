3dd14e2f605d117bf506f48c71fed2e1393a2dd8
Ryan Zolper <zolper11@gmail.com>
2015-03-26 10:32:52 -0400

    working on heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-@sample = User.create( name: 'Sample User', email: 'yadayada@yadoo.com',  password_digest: 'password', admin: true)
+@sample = User.create( name: 'Sample User', email: 'yada@yadoo.com',  password_digest: BCrypt::Password.create("pwd", cost: 4), admin: true)
 
 @sample.pins.create( title: 'Sample 1', description: 'yadayadyadaydaydayadya', user_id: '30', remote_pinimage_url: 'http://i.imgur.com/NJvCwj3.jpg')
 @sample.pins.create( title: 'Sample 2', description: 'yadayadyadaydaydayadya', user_id: '30', remote_pinimage_url: 'http://s.petrolicious.com/2013/features/god-in-bmw-2002/1974-bmw-2002-03.jpg')
